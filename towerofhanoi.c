f50904e2f8c400637ce0fb17974ddd87af09224d
11249a157-jpg <11249a157@kanchiuniv.ac.in>
2026-02-12 21:16:17 +0530

    Tower of Hanoi algorithm.c

---
+++ towerofhanoi.c
@@ -0,0 +1,25 @@
+DA#include<stdio.h>
+void towerofhanoi(int n,char s,char d,char t)
+{
+	if(n==1)
+	{
+		printf(" move the disk %d from %c to %c ",n,s,d);
+		return ;
+	}
+	towerofhanoi(n-1,s,t,d);
+	printf("\n move disk %d from %c to %c",n,s,d);
+	towerofhanoi(n,s, d, t);
+	printf("\n move disk %d from %c to %c",n,d,s);
+	towerofhanoi(n-1,t,d,s);
+
+}
+int main()
+{
+	int n;
+	char s,d,t;
+	printf("enter no.of disks: ");
+	scanf("%d",&n);
+	towerofhanoi(n,'s','d','t');
+	
+	
+}
