25115e82e7c9220c35042d852b1d9560f9929298
11249a157-jpg <11249a157@kanchiuniv.ac.in>
2026-04-26 16:42:21 +0530

    Tower of Hanoi.c

--- DAA/towerofhanoi.c
+++ DAA/towerofhanoi.c
@@ -1,25 +1,35 @@
 #include<stdio.h>
-void towerofhanoi(int n,char s,char d,char t)
-{
-	if(n==1)
-	{
-		printf(" move the disk %d from %c to %c ",n,s,d);
-		return ;
-	}
-	towerofhanoi(n-1,s,t,d);
-	printf("\n move disk %d from %c to %c",n,s,d);
-	towerofhanoi(n,s, d, t);
-	printf("\n move disk %d from %c to %c",n,d,s);
-	towerofhanoi(n-1,t,d,s);
 
+// function to solve Tower of Hanoi
+void towerofhanoi(int n, char s, char d, char t)
+{
+    // base case: only one disk
+    if(n == 1)
+    {
+        printf("Move disk %d from %c to %c\n", n, s, d);
+        return;
+    }
+
+    // move n-1 disks from source to temporary
+    towerofhanoi(n-1, s, t, d);
+
+    // move nth disk from source to destination
+    printf("Move disk %d from %c to %c\n", n, s, d);
+
+    // move n-1 disks from temporary to destination
+    towerofhanoi(n-1, t, d, s);
 }
+
 int main()
 {
-	int n;
-	char s,d,t;
-	printf("enter no.of disks: ");
-	scanf("%d",&n);
-	towerofhanoi(n,'s','d','t');
-	
-	
+    int n;
+
+    // input number of disks
+    printf("Enter no. of disks: ");
+    scanf("%d", &n);
+
+    // call function (s = source, d = destination, t = temporary)
+    towerofhanoi(n, 'S', 'D', 'T');
+
+    return 0;
 }
