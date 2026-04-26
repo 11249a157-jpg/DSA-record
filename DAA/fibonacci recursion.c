6c40f780d8ff9bd60395f627023d6218098caae3
11249a157-jpg <11249a157@kanchiuniv.ac.in>
2026-04-26 16:40:15 +0530

    Fibonacci recursion.c

--- DAA/fibonacci recursion.c
+++ DAA/fibonacci recursion.c
@@ -1,29 +1,44 @@
 #include <stdio.h>
+
+// recursive function to find nth Fibonacci number
 int fib(int n)
 {
-    if(n==0)
+    // base case: fib(0) = 0
+    if(n == 0)
     {
         return 0;
     }
-    if(n==1)
+
+    // base case: fib(1) = 1
+    if(n == 1)
     {
         return 1;
     }
-    return fib(n-1)+fib(n-2);
-   
+
+    // recursive case: fib(n) = fib(n-1) + fib(n-2)
+    return fib(n-1) + fib(n-2);
 }
+
 int main()
 {
-    int n,result;
+    int n, result;
+
+    // take input from user
     printf("Enter the value of n: ");
-    scanf("%d",&n);
-    if(n<0)
+    scanf("%d", &n);
+
+    // check for invalid input
+    if(n < 0)
     {
         printf("Invalid number");
         return 0;
     }
-   
-    result=fib(n);
-    printf("Fibonacci term :%d",result);
+
+    // call fibonacci function
+    result = fib(n);
+
+    // print result
+    printf("Fibonacci term : %d", result);
+
     return 0;
 }
